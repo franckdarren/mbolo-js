0bf9386f0dadc0c36a9ec733ca08c7ffe6b4591d
DarrenNills <kikondanze@gmail.com>
2024-06-20 11:41:54 +0100

    Episode 3

---
+++ js/episode-3.js
@@ -0,0 +1,9 @@
+// Demander de saisir le premier nombre
+let nombre1 = prompt("Veuillez saisir le premier nombre");
+
+// Demander de saisir le deuxième nombre
+let nombre2 = prompt("Veuillez saisir le deuxième nombre");
+
+// Afficher la somme des deux nombres en alerte
+let somme = parseInt(nombre1) + parseInt(nombre2);
+alert("La somme des deux nombres est : " + somme);
