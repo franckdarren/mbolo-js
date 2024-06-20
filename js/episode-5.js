8770a4c79ee0df82ce878d4eb9c715cf04eefef2
DarrenNills <kikondanze@gmail.com>
2024-06-20 13:03:07 +0100

    Episode 5

---
+++ js/episode-5.js
@@ -0,0 +1,5 @@
+// Demander à l'utilisateur de saisir un nombre
+let nombre = parseInt(prompt("Saisissez un nombre : "));
+
+// Afficher le double du nombre dans une alerte
+alert("Le double du nombre que vous avez saisi est : " + nombre*2);
