a00c78684ce8f65cb8cf168da37b5f6619495ccd
DarrenNills <kikondanze@gmail.com>
2024-06-20 13:18:06 +0100

    Episode 7

---
+++ js/episode-7.js
@@ -0,0 +1,10 @@
+// Demander à l'utilisateur de saisir son age
+let age = parseInt(prompt("Entrez votre âge : "));
+
+if (age>=18) {
+    // console.log('Vous êtes majeur')
+    alert('Vous êtes majeur')
+} else {
+    // console.log('Vous êtes mineur')
+    alert('Vous êtes mineur')
+}
