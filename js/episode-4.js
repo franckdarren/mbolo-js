142096c41504711f049636f013b4060da33ee018
DarrenNills <kikondanze@gmail.com>
2024-06-20 12:59:04 +0100

    Episode 4

---
+++ js/episode-4.js
@@ -0,0 +1,10 @@
+let valeur1 = 1;
+let valeur2 = 2;
+
+// Permutation des valeurs
+let temp = valeur1;
+valeur1 = valeur2;
+valeur2 = temp
+
+// alert("valeur 1 = " + valeur2 + " et la valeur 2 = " + valeur1)
+alert("Valeur 1 = " + valeur1 + "  ;  " + "Valeur 2 = " + valeur2);
