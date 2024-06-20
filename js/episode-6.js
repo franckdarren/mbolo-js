804c75f2244daead5cd5771a60eeb0a72ffb6960
DarrenNills <kikondanze@gmail.com>
2024-06-20 13:08:55 +0100

    Episode 6

---
+++ js/episode-6.js
@@ -0,0 +1,7 @@
+// Demander à l'utilisateur de saisir 3 nombres
+let nombre1 = parseInt(prompt("Veuillez saisir le 1er nombre"));
+let nombre2 = parseInt(prompt("Veuillez saisir le 2eme nombre"));
+let nombre3 = parseInt(prompt("Veuillez saisir le 3eme nombre"));
+
+// Calcule de la moyenne
+alert("La moyenne des nombres saisis est : " + (nombre1+nombre2+nombre3)/3);
